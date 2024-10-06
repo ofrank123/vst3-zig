8ca1bc338947094256615cfc661fc664fb5226d3
Samuel Edwards-Marsh <sam.david.marsh@gmail.com>
2024-10-06 12:52:35 -0700

    shortened build.zig

--- build.zig
+++ build.zig
@@ -1,15 +1,11 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-
-    const optimize = b.standardOptimizeOption(.{});
-
     const lib = b.addSharedLibrary(.{
         .name = "HelloVST3",
         .root_source_file = b.path("src/plugin.zig"),
-        .target = target,
-        .optimize = optimize,
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
     });
     lib.linkLibC();
 
